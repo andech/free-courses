6f769adb9413ef6c170be046702e0628f0f5dc04
andech <7578191@gmail.com>
2018-06-02 17:54:48 +0300

    Initial commit

--- task2.rb
+++ task2.rb
@@ -1,3 +1,21 @@
 # Task 2
+require 'date'
 
-# Put your code here
+month = {"января"    => 1, 
+         "февраля"   => 2, 
+         "марта"     => 3, 
+         "апреля"    => 4, 
+         "мая"       => 5, 
+         "июня"      => 6, 
+         "июля"      => 7, 
+         "августа"   => 8, 
+         "сентября"  => 9, 
+         "октября"   => 10, 
+         "ноября"    => 11, 
+         "декабря"   => 12}  
+
+current_date = DateTime.new(ARGV[2].to_i, month[ARGV[1]], ARGV[0].to_i)
+new_year_date = DateTime.new(current_date.year, 12, 31)
+result = new_year_date - current_date
+
+puts result.to_i
