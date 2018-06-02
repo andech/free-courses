4d8cb9a363bfdb83102bd8701c6dc83929d4bee0
andech <7578191@gmail.com>
2018-06-02 20:07:06 +0300

    Initial commit

--- task3.rb
+++ task3.rb
@@ -1,3 +1,31 @@
 # Task 3
+seconds = ARGV[0].to_i + ARGV[1].to_i
+hours = seconds / 3600
+minutes = seconds / 60 - hours * 60
+seconds -= minutes * 60 + hours * 3600
+result = ""
 
-# Put your code here
+def func(number, a, b, c) 
+  last_digit = number % 10
+  if last_digit == 1
+    "#{number} #{a}"
+  elsif last_digit > 1 && last_digit < 5 && number / 10 != 1
+    "#{number} #{b}"
+  else 
+    "#{number} #{c}"
+  end
+end
+
+if hours.nonzero? 
+  result += func(hours, "час ", "часа ", "часов ")
+end
+
+if minutes.nonzero?
+  result += func(minutes, "минута ", "минуты ", "минут ")
+end
+
+if seconds.nonzero?
+  result += func(seconds, "секунда", "секунды", "секунд")
+end
+
+puts result
