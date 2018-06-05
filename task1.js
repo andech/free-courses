e372d297ba40333fb46473b62a28e9d7b67eb22b
andech <7578191@gmail.com>
2018-06-05 17:30:47 +0300

    Change to stdout

--- task1.js
+++ task1.js
@@ -1,6 +1,6 @@
 var str = process.argv[2].toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
 if (str === str.split('').reverse().join('')) {
-  console.log("YES");
+  process.stdout.write("YES");
 } else {
-  console.log("NO");
+  process.stdout.write("NO");
 }
