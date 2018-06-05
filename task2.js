1fee5ac19357b261360b56dfa2a70097fb28fa94
andech <7578191@gmail.com>
2018-06-05 19:24:52 +0300

    Initial commit

---
+++ task2.js
@@ -0,0 +1,18 @@
+var month = {"января": 0,
+             "февраля": 1,
+             "марта": 2,
+             "апреля": 3,
+             "мая": 4,
+             "июня": 5,
+             "июля": 6,
+             "августа": 7,
+             "сентября": 8,
+             "октября": 9,
+             "ноября": 10,
+             "декабря": 11};
+
+var currentDate = new Date(parseInt(process.argv[4]), month[process.argv[3]], parseInt(process.argv[2]));
+var newYearDate = new Date(currentDate.getFullYear(), 11, 31);
+var result = (newYearDate - currentDate) / 1000 / 60 / 60 / 24;
+
+process.stdout.write(String(result));
